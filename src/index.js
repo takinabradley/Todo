88458294b4a3e2dac91a5f64b81ae54d647c21a1
takinabradley <bradfinney98@gmail.com>
2021-12-05 11:11:48 -0500

    started project and installed dependecies

---
+++ src/index.js
@@ -0,0 +1,90 @@
+/*let Events = {
+  events: {},
+  on: function (eventName, fn) {
+    this.events[eventName] = this.events[eventName] || [];
+    this.events[eventName].push(fn);
+  },
+  off: function(eventName, fn) {
+    if (this.events[eventName]) {
+      for (let i = 0; i < this.events[eventName].length; i++) {
+        if (this.events[eventName][i] === fn) {
+          this.events[eventName].splice(i, 1);
+          break;
+        }
+      }
+    }
+  },
+  emit: function (eventName, data) {
+    if (this.events[eventName]) {
+      this.events[eventName].forEach(function(fn) {
+        fn(data);
+      });
+    }
+  },
+};*/
+
+
+  /*
+    Classic terminology:
+      pubsub
+      pubsub.subscribe
+      pubsub.unsubscribe
+      pubsub.publish    
+                        */
+
+let Events = (function() {
+  events = {};
+
+  function on (eventName, fn) {
+    events[eventName] = events[eventName] || [];
+    events[eventName].push(fn);
+  }
+
+  function off (eventName, fn) {
+    if (events[eventName]) {
+      for (let i = 0; i < events[eventName].length; i++) {
+        if (events[eventName][i] === fn) {
+          events[eventName].splice(i, 1);
+          break;
+        }
+      }
+    }
+  }
+
+  function emit(eventName, data) {
+    if (events[eventName]) {
+      events[eventName].forEach(function(fn) {
+        fn(data);
+      });
+    }
+  }
+
+  return {on, off, emit};
+})();
+
+
+
+const people = (function() {
+  let people = [];
+  function addPeople(array) {
+    array.forEach(person => people.push(person));
+    Events.emit('peopleChanged', people);
+  }
+  return {addPeople};
+})();
+
+
+const module2 = (function() {
+  let stats = {};
+
+  Events.on('peopleChanged', setPeople);
+
+  function setPeople(people) {
+    stats.people = people.length;
+    console.log(stats);
+    console.log(stats.people);
+  }
+})();
+
+people.addPeople(['brian', 'g', 'the rock']);
+
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Todo',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+     {
+       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+       type: 'asset/resource',
+     },
+    ],
+  },
+};
