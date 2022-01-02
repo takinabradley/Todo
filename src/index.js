d5c75f5f5fd212b8e782916e0b4036ee71d2ce00
takinabradley <bradfinney98@gmail.com>
2022-01-02 04:55:40 -0500

    Made editing optional when rendering projects. Created a way to add, edit, and remove projects from the webpage. The same still needs to be done for todos.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import stylesheet from './styles.css'
-import Projects, {ProjectRenderer, ProjectInterface} from './modules/projects.js';
-import Events from './modules/events.js';
-
-
+import Projects, {ProjectRenderer} from './modules/projects.js';
 
 
 ProjectRenderer.init(document.querySelector('.projects-list'), 
-                     document.querySelector('.todo-list'))
+                     document.querySelector('.todo-list'),
+                     true)
 
 Projects.getFromLocalStorage();
 
-
-
-
+/* Testing projects:
+Projects.add('name').find('name').addTodo('Change my name!', "Click Edit next to my project's name to change it! You may also remove the project, or add a new one from the sidebar while you're at it!", 'never', 'low')
+*/
 //ToDo objects should have properties such as title, description, duedate, priority.
 //Notes and checklist would be nice too.
 
--- src/modules/projects.js
+++ src/modules/projects.js
@@ -1,19 +1,12 @@
 import Events from "./events.js";
 
-/*
-const config = {
-
-}
-*/
 /*  
     This module exports a 'Projects' object that keeps 
     track of Projects and their Todos.
 
     Note:
       Projects.add & Projects.remove return the Projects module itself.
-      .addTodo and .removeTodo used on project objects returns that object in
-      the list.
-      .edit used on todo objects returns project object it belongs to.
+      .addTodo, .removeTodo, and .edit return the project the todo belongs to.
 
     ~~~~~~~~~~~~~~~~~~~Add, retrive, and remove projects~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -27,7 +20,6 @@ const config = {
       Projects may not have identical names.
 
       Remove projects with Project.remove('projectName')
-
 
     ~~~~~~~~~~~~~~~~~~~Add, retrive, and remove todos~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -52,20 +44,16 @@ const config = {
         Projects.find('ProjectName').findTodo('todoName').edit('title', 'newTitle')
 */
 
-/*
-enableEvents: true
-renderTodosOnClick: true
-*/
-
 
 const Projects = (function() {
   const projectsList = [];
+
   function _saveToLocalStorage() {
-    localStorage.setItem('projectsList', JSON.stringify(projectsList))
+    localStorage.setItem('projectsList', JSON.stringify(projectsList));
   };
 
   function getFromLocalStorage() {
-    let storage = JSON.parse(localStorage.getItem('projectsList'))
+    let storage = JSON.parse(localStorage.getItem('projectsList'));
     if(storage === null) return; //returns if localStorage hasn't been used
 
     for(let i = 0; i < storage.length; i++) {
@@ -83,17 +71,17 @@ const Projects = (function() {
   
   /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~Project Creation~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
   function add(name) {
-    if(name === undefined) {
-      return alert('Projects must have a name!')
+    if(name === undefined || /^\s*$/.test(name)) { //returns if string is empty or only contains whitespace
+      return console.log('Projects must have a name!');
     } else if(find(name) !== undefined) {
-      return alert('Projects cannot have identical names')
+      return alert('Projects cannot have identical names');
     }
 
     const todoList = [];
     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~Todo Creation~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
     function addTodo(title, description = '', duedate = '', priority = '') {
-      if(title === undefined) {
-        return alert('Todos must have a title!')
+      if(title === undefined || title === '') {
+        return alert('Todos must have a title!');
       }else if(findTodo(title) !== undefined) {
         return alert('Todos cannot have identical titles');
       }
@@ -104,30 +92,29 @@ const Projects = (function() {
         if(field === undefined || change === undefined) {
           return alert('Missing arguments!');
         } else if (field === 'title') {
+
           if(title === undefined) {
-            return alert('Todos must have a title!')
+            return alert('Todos must have a title!');
           }else if(findTodo(change) !== undefined) {
             return alert('Todos cannot have identical titles');
           }
           title = change;
-          //return change;
+
         } else if(field === 'description') {
           description = change;
-          //return change;
         } else if(field === 'duedate') {
           duedate = change;
-          //return change;
         } else if(field === 'priority') {
           priority = change;
-          //return change;
         } else {
           return alert('Field does not exist!');
         }
-        _saveToLocalStorage()
-        Events.emit('projectsUpdate', [...projectsList])
+
+        _saveToLocalStorage();
+        Events.emit('projectsUpdate');
         return find(parentProject);
       }
-      //todo object
+      //Todo object
       const todo = Object.freeze({
         get title() {return title}, 
         get description() {return description}, 
@@ -138,12 +125,12 @@ const Projects = (function() {
       });
       todoList.push(todo);
       _saveToLocalStorage();
-      Events.emit('projectsUpdate', [...projectsList])
+      Events.emit('projectsUpdate');
       return this;
     };
     /*~~~~~~~~~~~~~~~~~~~~~~~~~End of Todo Creation~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
     
-    //other crud
+    //other project crud
     function findTodo(title) {
       if(title === undefined) {
         return alert('Todo not found!');
@@ -159,24 +146,24 @@ const Projects = (function() {
         return alert('Todo not found!');
       } else {
         todoList.splice(index , 1);
-        _saveToLocalStorage()
-        Events.emit('projectsUpdate', [...projectsList])
+        _saveToLocalStorage();
+        Events.emit('projectsUpdate');
         return this;
       }
     }
 
-    //project object
+    //Project object
     const project = Object.freeze({ 
       get name() {return name}, 
       set name(newName) {
-        if(newName === undefined) {
-          return alert('Projects must have a name!')
+        if(newName === undefined || /^\s*$/.test(newName)) {
+          return alert('Projects must have a name!');
         } else if(find(newName) !== undefined) {
           return alert('Projects cannot have identical names');
         } else {
           name = newName;
           _saveToLocalStorage();
-          Events.emit('projectsUpdate', [...projectsList]);
+          Events.emit('projectsUpdate');
         }
       },
       get listTodos() {return [...todoList]},
@@ -185,13 +172,13 @@ const Projects = (function() {
       removeTodo
     });
     projectsList.push(project);
-    _saveToLocalStorage()
-    Events.emit('projectsUpdate', [...projectsList])
+    _saveToLocalStorage();
+    Events.emit('projectsUpdate');
     return this;
   };
   /*~~~~~~~~~~~~~~~~~~~~~~~~End of Project Creation~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-  //other crud
+  //other module crud
   function remove(name) {
     const index = projectsList.findIndex(project => project.name === name);
 
@@ -199,8 +186,8 @@ const Projects = (function() {
       return alert('Project not found!'); //returns if findIndex doesn't find a match
     } else {
       projectsList.splice(index , 1);
-      _saveToLocalStorage()
-      Events.emit('projectsUpdate', [...projectsList])
+      _saveToLocalStorage();
+      Events.emit('projectsUpdate');
       return this;
     }
   }
@@ -225,147 +212,206 @@ const Projects = (function() {
   }
 })();
 
-/*Should init with areas to render to before manipulating the Projects module.
-Not doing so will cause ProjectRenderer to not render until a "projectsUpdate"
-event. Other methods will also be inaccessable until after initiation.
+
+/*Renders Projects on the webpage. 
+Use ProjectRenderer.init(projectElement, todoElement, editable) to set the areas
+to render to, and whether they may be edited. Todos will be rendered when their
+respective projects are clicked.
 If you'd like to only render project names, you may pass a dummy non-object 
-value as the todoElement and it will fail quietly with a console message by
+value as the todoElement. It will fail quietly with a console message by
 default.
-You can optionally set Events to false if don't want projects and todos to 
-automatically render on "projectsUpdate" events.*/
+
+After init, ProjectRenderer will have two methods available to it:
+ProjectRenderer.setConfig: Used exactly the same as ProjectRenderer.init
+ProjectRenderer.off: resets ProjectRenderer to it's original state and clears 
+elements immediately. */
 
 
 const ProjectRenderer = (function() {
   let projectArea;
   let todoArea;
+  let renderEditable;
+  let firstInit = true;
 
-  function _exportElements() { return [projectArea, todoArea] }
-  Events.on('requestElements', _exportElements)
+  function _renderProjectAdder() {
+    const form = document.createElement('form');
+    form.classList.add('project-form');
+    projectArea.appendChild(form);
 
-  function init(projectElement, todoElement, useEvents = true) {
-    projectArea = projectElement;
-    todoArea = todoElement;
+    const addLabel = document.createElement('label');
+    addLabel.classList.add('project-name-label');
+    addLabel.setAttribute('for', 'name');
+    addLabel.textContent = "Project Name:";
+    form.appendChild(addLabel);
 
-    this.renderProjects = renderProjects;
-    this.renderTodos = renderTodos;
+    const addInput = document.createElement('input');;
+    addInput.classList.add('project-name-input');
+    addInput.setAttribute('name', 'name');
+    addInput.setAttribute('type', 'text');
+    addInput.setAttribute('placeholder', 'New Project');
+    addInput.setAttribute('required', '');
+    form.appendChild(addInput);
 
-    /*Putting Events code here allows events to be triggered only after init, 
-    at the cost of not garbage collecting this function*/
-    if(useEvents === true) {
-      Events.on('projectsUpdate', this.renderProjects)
-    }
-    delete this.init;
-  } 
+    const addBtn = document.createElement('button');
+    addBtn.classList.add('project-form-addBtn');
+    addBtn.setAttribute('type', 'submit');
+    addBtn.textContent = 'Add';
+    form.appendChild(addBtn);
 
-  function renderProjects() {
+    const addProject = (e) => Projects.add(addInput.value);
+    addBtn.addEventListener('click', addProject);
+  }
+  
+  function _renderProjectEditor(projectElement) {
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('project-edit');
+    editBtn.textContent = '✎';
+    projectElement.appendChild(editBtn)
+
+    //Hides edit button and provides additional options until user moves away
+    editBtn.addEventListener('click', function(e) {
+      const projectNameElement = e.target.nextSibling
+      const projectName = projectNameElement.textContent
+      
+      projectNameElement.setAttribute('contenteditable', 'true');
+      editBtn.setAttribute('hidden', '')
+
+      const removeElement = document.createElement('span');
+      removeElement.classList.add('project-remove');
+      removeElement.textContent = '-';
+      projectElement.insertBefore(removeElement, projectNameElement);
+
+      removeElement.addEventListener('click', () => Projects.remove(projectName));
+      
+      const exitEditor = (e) => {
+        projectNameElement.setAttribute('contenteditable', 'false')
+        projectElement.removeChild(removeElement)
+        editBtn.removeAttribute('hidden')
+        
+        if(projectNameElement.textContent !== projectName) {
+          const newName = projectNameElement.textContent;
+          Projects.find(projectName).name = newName;
+
+          //checks if old project name exists, and resets textContent if still does
+          if(typeof Projects.find(projectName) === 'object') {
+            projectNameElement.textContent = projectName;
+          }
+        }
+
+        //must remove listener, or will continue attempting to remove removeElement
+        projectElement.removeEventListener('mouseleave', exitEditor)
+      }
+
+      projectElement.addEventListener('mouseleave', exitEditor);
+    })
+  }
+
+  function _renderProjects() {
     if(typeof projectArea !== 'object') {
       console.log('ProjectRenderer: projectArea left undefined!');
       return;
     }
 
     projectArea.innerHTML = '';
-  
-    Projects.list.forEach(project => {
-      const projectElement = document.createElement('div')
-      projectElement.textContent = project.name 
-      projectElement.classList.add('project-element')
-      projectArea.appendChild(projectElement);
+    
+    if(renderEditable === true) _renderProjectAdder();
+
+    Projects.list.forEach( project => {
+      const projectElement = document.createElement('div');
+      projectElement.classList.add('project-element');
       
-      const clickHandler = (e) => renderTodos(e.target.textContent)
-      projectElement.addEventListener('click', clickHandler)
+      if(renderEditable === true) _renderProjectEditor(projectElement);
+
+      const name = document.createElement('span');
+      name.classList.add('project-name');
+      name.textContent = project.name;
+      projectElement.appendChild(name);
+
+      name.addEventListener('click', (e) => _renderTodos(e.target.textContent));
+
+      projectArea.appendChild(projectElement);
     })
   }
 
-  function renderTodos(projectName) {
+  
+  function _renderTodos(projectName) {
     if(typeof todoArea !== 'object') {
       console.log('ProjectRenderer: todoArea left undefined!');
       return;
     }
+
     todoArea.innerHTML = '';
   
-    const todos = Projects.find(projectName).listTodos
+    const todos = Projects.find(projectName).listTodos;
   
     if(todos.length > 0) {
   
       todos.forEach(todo => {
         const todoElement = document.createElement('div');
-        todoElement.classList.add('todo-element')
+        todoElement.classList.add('todo-element');
   
-        const title = document.createElement('span')
-        title.textContent = todo.title
-        title.classList.add('todo-title')
+        const title = document.createElement('span');
+        title.textContent = todo.title;
+        title.classList.add('todo-title');
   
-        const description = document.createElement('span')
-        description.textContent = todo.description
-        description.classList.add('todo-description')
+        const description = document.createElement('span');
+        description.textContent = todo.description;
+        description.classList.add('todo-description');
   
-        const duedate = document.createElement('span')
-        duedate.textContent = todo.duedate
-        duedate.classList.add('todo-duedate')
+        const duedate = document.createElement('span');
+        duedate.textContent = todo.duedate;
+        duedate.classList.add('todo-duedate');
   
-        const priority = document.createElement('span')
-        priority.textContent = todo.priority
-        priority.classList.add('todo-priority')
+        const priority = document.createElement('span');
+        priority.textContent = todo.priority;
+        priority.classList.add('todo-priority');
   
-        todoElement.appendChild(title)
-        todoElement.appendChild(description)
-        todoElement.appendChild(duedate)
-        todoElement.appendChild(priority)
+        todoElement.appendChild(title);
+        todoElement.appendChild(description);
+        todoElement.appendChild(duedate);
+        todoElement.appendChild(priority);
         todoArea.appendChild(todoElement);
       })
     }
   }
 
-  return {init}
-})();
+  function off() {
+    Events.off('projectsUpdate', _renderProjects);
 
-/*renders additional controls for managing the Projects module from the webpage. 
-Uses the elements selected for ProjectRenderer*/
-const ProjectInterface = (function() {
+    projectArea.innerHTML = '';
+    todoArea.innerHTML = '';
 
+    projectArea = undefined;
+    todoArea = undefined;
+    renderEditable = undefined;
+    firstInit = true;
+    
+    this.init = init;
+    delete this.setRenderConfig;
+    delete this.off;
+  }
+
+  function init(projectElement, todoElement, editable = true) {
+    projectArea = projectElement;
+    todoArea = todoElement;
+    renderEditable = editable;
+    _renderProjects();
+
+    if(firstInit === true) {
+      /*Putting Events code here allows events to be triggered only after init, 
+      at the cost of creating a closure.*/
+      Events.on('projectsUpdate', _renderProjects);
+
+      //reuses this function to set options after init if desired.
+      firstInit = false;
+      this.setConfig = init;
+      this.off = off;
+    }
+    delete this.init;
+  }
+  
+  return {init}; //after init: {setConfig: init, off};
 })();
 
 export default Projects;
-export {ProjectRenderer, ProjectInterface}
-
-/* Random other functions I may need
-function getTodoIndex(projectName, todoTitle) {
-  let array = Projects.find(projectName).listTodo
-  let todoIndex;
-  array.forEach( (item, index) => {
-    if(item.title === todoTitle) {
-      todoIndex = index; 
-    }
-  });
-  return todoIndex;
-}
-
-function getProjectIndex(projectName) {
-  let projectIndex 
-  Projects.list.forEach( (item, index) => {
-    if(item.name === projectName) {
-      projectIndex = index;
-    }
-  });
-  return projectIndex;
-}
-*/
-
-
-//Possible extra functionality
-//If you want to add additional methods and properties to the module or 
-//projects, you can do so by attaching them to their prototype here:
-
-//I need to re-create this functionality
-// Custom Methods for the entire 'Projects' Object go here:
-//Use: Projects.CustomMethod()
-/*  ProjectsPrototype = {
-    method: function(){};
-  } */
-
-
-//Custom methods for projects inside the 'projects' array go here:
-//Use: Projects.get('project').CustomMethod()
-/*  projectsPrototype = {
-      method: function() {}
-    } */
+export {ProjectRenderer}
